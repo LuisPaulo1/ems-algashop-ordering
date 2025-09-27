15abf7ba563330b2e578da8a7e8bd1d5935b2d4f
LuisPaulo1 <csluispaulo@gmail.com>
2025-09-27 19:25:32 -0300

    test: Expand OrderTest with additional tests for order initialization and item addition
    - Added test for verifying order initialization with default attributes.
    - Introduced test for validating the `addItem` method behavior, covering item properties and order updates.

--- src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
@@ -1,13 +1,50 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
+import com.algaworks.algashop.ordering.domain.valueobject.Money;
+import com.algaworks.algashop.ordering.domain.valueobject.ProductName;
+import com.algaworks.algashop.ordering.domain.valueobject.Quantity;
 import com.algaworks.algashop.ordering.domain.valueobject.id.CustomerId;
+import com.algaworks.algashop.ordering.domain.valueobject.id.ProductId;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
     @Test
-    public void shouldGenerate() {
+    void shouldGenerate() {
         Order order = Order.draft(new CustomerId());
+
+        Assertions.assertThat(order.id()).isNotNull();
+        Assertions.assertThat(order.status()).isEqualTo(OrderStatus.DRAFT);
+        Assertions.assertThat(order.customerId()).isNotNull();
+        Assertions.assertThat(order.totalAmount()).isEqualTo(Money.ZERO);
+        Assertions.assertThat(order.totalItems()).isEqualTo(Quantity.ZERO);
+        Assertions.assertThat(order.items()).isEmpty();
+
     }
-  
+
+    @Test
+    void shouldAddItem() {
+        Order order = Order.draft(new CustomerId());
+        ProductId productId = new ProductId();
+
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(1)
+        );
+
+        Assertions.assertThat(order.items()).hasSize(1);
+
+        OrderItem orderItem = order.items().iterator().next();
+
+        Assertions.assertWith(orderItem,
+                i -> Assertions.assertThat(i.id()).isNotNull(),
+                i -> Assertions.assertThat(i.productName()).isEqualTo(new ProductName("Mouse pad")),
+                i -> Assertions.assertThat(i.productId()).isEqualTo(productId),
+                i -> Assertions.assertThat(i.price()).isEqualTo(new Money("100")),
+                i -> Assertions.assertThat(i.quantity()).isEqualTo(new Quantity(1))
+        );
+    }
 }
