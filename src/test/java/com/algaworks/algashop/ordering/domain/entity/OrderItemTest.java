e0226c35cd9a293b03265ffb83c4fb5375400f93
LuisPaulo1 <csluispaulo@gmail.com>
2025-10-01 20:52:01 -0300

    test: Refactor tests to use `ProductTestDataBuilder` and streamline product setup
    - Replaced hardcoded product setup in `Order` and `OrderItem` tests with `ProductTestDataBuilder`.
    - Simplified test cases for adding items and calculating totals by utilizing reusable product builders.
    - Adjusted assertions to align with changes in product data setup.

--- src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderItemTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderItemTest.java
@@ -1,22 +1,17 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
-import com.algaworks.algashop.ordering.domain.valueobject.Money;
-import com.algaworks.algashop.ordering.domain.valueobject.ProductName;
 import com.algaworks.algashop.ordering.domain.valueobject.Quantity;
 import com.algaworks.algashop.ordering.domain.valueobject.id.OrderId;
-import com.algaworks.algashop.ordering.domain.valueobject.id.ProductId;
 import org.junit.jupiter.api.Test;
 
 class OrderItemTest {
 
     @Test
-    public void shouldGenerate() {
+    void shouldGenerate() {
         OrderItem.brandNew()
-                .productId(new ProductId())
+                .product(ProductTestDataBuilder.aProduct().build())
                 .quantity(new Quantity(1))
                 .orderId(new OrderId())
-                .productName(new ProductName("Mouse pad"))
-                .price(new Money("100"))
                 .build();
     }
 
--- src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
@@ -16,21 +16,18 @@ class OrderTest {
     @Test
     void shouldAddItem() {
         Order order = Order.draft(new CustomerId());
-        ProductId productId = new ProductId();
+        Product product = ProductTestDataBuilder.aProductAltMousePad().build();
+        ProductId productId = product.id();
 
-        order.addItem(
-                productId,
-                new ProductName("Mouse pad"),
-                new Money("100"),
-                new Quantity(1)
-        );
+        order.addItem(product, new Quantity(1));
 
         Assertions.assertThat(order.items()).hasSize(1);
+
         OrderItem orderItem = order.items().iterator().next();
 
         Assertions.assertWith(orderItem,
                 i -> Assertions.assertThat(i.id()).isNotNull(),
-                i -> Assertions.assertThat(i.productName()).isEqualTo(new ProductName("Mouse pad")),
+                i -> Assertions.assertThat(i.productName()).isEqualTo(new ProductName("Mouse Pad")),
                 i -> Assertions.assertThat(i.productId()).isEqualTo(productId),
                 i -> Assertions.assertThat(i.price()).isEqualTo(new Money("100")),
                 i -> Assertions.assertThat(i.quantity()).isEqualTo(new Quantity(1))
@@ -40,14 +37,9 @@ class OrderTest {
     @Test
     void shouldGenerateExceptionWhenTryToChangeItemSet() {
         Order order = Order.draft(new CustomerId());
-        ProductId productId = new ProductId();
+        Product product = ProductTestDataBuilder.aProductAltMousePad().build();
 
-        order.addItem(
-                productId,
-                new ProductName("Mouse pad"),
-                new Money("100"),
-                new Quantity(1)
-        );
+        order.addItem(product, new Quantity(1));
 
         Set<OrderItem> items = order.items();
 
@@ -58,23 +50,18 @@ class OrderTest {
     @Test
     void shouldCalculateTotals() {
         Order order = Order.draft(new CustomerId());
-        ProductId productId = new ProductId();
 
         order.addItem(
-                productId,
-                new ProductName("Mouse pad"),
-                new Money("100"),
+                ProductTestDataBuilder.aProductAltMousePad().build(),
                 new Quantity(2)
         );
 
         order.addItem(
-                productId,
-                new ProductName("RAM Memory"),
-                new Money("50"),
+                ProductTestDataBuilder.aProductAltRamMemory().build(),
                 new Quantity(1)
         );
 
-        Assertions.assertThat(order.totalAmount()).isEqualTo(new Money("250"));
+        Assertions.assertThat(order.totalAmount()).isEqualTo(new Money("400"));
         Assertions.assertThat(order.totalItems()).isEqualTo(new Quantity(3));
     }
 
@@ -202,9 +189,7 @@ class OrderTest {
         Order order = Order.draft(new CustomerId());
 
         order.addItem(
-                new ProductId(),
-                new ProductName("Destkop X11"),
-                new Money("10.00"),
+                ProductTestDataBuilder.aProductAltMousePad().build(),
                 new Quantity(3)
         );
 
@@ -213,7 +198,7 @@ class OrderTest {
         order.changeItemQuantity(orderItem.id(), new Quantity(5));
 
         Assertions.assertWith(order,
-                o -> Assertions.assertThat(o.totalAmount()).isEqualTo(new Money("50.00")),
+                o -> Assertions.assertThat(o.totalAmount()).isEqualTo(new Money("500")),
                 o -> Assertions.assertThat(o.totalItems()).isEqualTo(new Quantity(5))
         );
     }
