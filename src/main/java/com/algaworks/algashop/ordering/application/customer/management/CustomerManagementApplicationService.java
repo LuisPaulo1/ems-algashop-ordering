471cf7a98c92fb7c3df6bfaa76cd0f435633ed12
LuisPaulo1 <csluispaulo@gmail.com>
2025-11-21 17:36:15 -0300

    feat: Add customer update functionality with new mapping support
    - Introduced `update` method in `CustomerManagementApplicationService` for updating customer details with `CustomerUpdateInput`.
    - Integrated `Mapper` for simplified object mapping in `findById` method.
    - Added `CustomerUpdateInput` and corresponding TestDataBuilder for test purposes.
    - Enhanced integration tests to validate customer update scenarios.

--- src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.application.customer.management;
 
 import com.algaworks.algashop.ordering.application.commons.AddressData;
+import com.algaworks.algashop.ordering.application.utility.Mapper;
 import com.algaworks.algashop.ordering.domain.model.commons.*;
 import com.algaworks.algashop.ordering.domain.model.customer.*;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,8 @@ public class CustomerManagementApplicationService {
 
     private final CustomerRegistrationService customerRegistration;
     private final Customers customers;
+
+    private final Mapper mapper;
 
     @Transactional
     public UUID create(CustomerInput input) {
@@ -50,29 +53,40 @@ public class CustomerManagementApplicationService {
         Objects.requireNonNull(customerId);
         Customer customer = customers.ofId(new CustomerId(customerId))
                 .orElseThrow(CustomerNotFoundException::new);
-        return CustomerOutput.builder()
-                .id(customer.id().value())
-                .firstName(customer.fullName().firstName())
-                .lastName(customer.fullName().lastName())
-                .email(customer.email().value())
-                .document(customer.document().value())
-                .phone(customer.phone().value())
-                .promotionNotificationsAllowed(customer.isPromotionNotificationsAllowed())
-                .loyaltyPoints(customer.loyaltyPoints().value())
-                .registeredAt(customer.registeredAt())
-                .archived(customer.isArchived())
-                .archivedAt(customer.archivedAt() != null ? customer.archivedAt() : null)
-                .birthDate(customer.birthDate() != null ? customer.birthDate().value() : null)
-                .address(AddressData.builder()
-                        .street(customer.address().street())
-                        .number(customer.address().number())
-                        .complement(customer.address().complement())
-                        .neighborhood(customer.address().neighborhood())
-                        .city(customer.address().city())
-                        .state(customer.address().state())
-                        .zipCode(customer.address().zipCode().value())
-                        .build())
-                .build();
+
+        return mapper.convert(customer, CustomerOutput.class);
+    }
+
+    @Transactional
+    public void update(UUID rawCustomerId, CustomerUpdateInput input) {
+        Objects.requireNonNull(input);
+        Objects.requireNonNull(rawCustomerId);
+
+        Customer customer = customers.ofId(new CustomerId(rawCustomerId))
+                .orElseThrow(CustomerNotFoundException::new);
+
+        customer.changeName(new FullName(input.getFirstName(), input.getLastName()));
+        customer.changePhone(new Phone(input.getPhone()));
+
+        if (Boolean.TRUE.equals(input.getPromotionNotificationsAllowed())) {
+            customer.enablePromotionNotifications();
+        } else {
+            customer.disablePromotionNotifications();
+        }
+
+        AddressData address = input.getAddress();
+
+        customer.changeAddress(Address.builder()
+                .zipCode(new ZipCode(address.getZipCode()))
+                .state(address.getState())
+                .city(address.getCity())
+                .neighborhood(address.getNeighborhood())
+                .street(address.getStreet())
+                .number(address.getNumber())
+                .complement(address.getComplement())
+                .build());
+
+        customers.add(customer);
     }
 
 }
