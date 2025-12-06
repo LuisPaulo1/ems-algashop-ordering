bf6c888ebdc9ab31aaa8ea144744061be44457b4
LuisPaulo1 <csluispaulo@gmail.com>
2025-12-06 19:31:40 -0300

    feat: Add domain event publishing for `Order` lifecycle changes
    - Introduced `OrderPlacedEvent`, `OrderPaidEvent`, `OrderReadyEvent`, and `OrderCanceledEvent` for `Order` state transitions.
    - Enhanced `Order` entity to publish domain events during lifecycle changes.
    - Added `OrderEventListener` to handle domain events.
    - Updated integration tests to verify event publishing and listener interactions.

--- src/main/java/com/algaworks/algashop/ordering/domain/model/order/Order.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/order/Order.java
@@ -16,7 +16,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class Order extends AbstractEventSourceEntity
+public class Order
+        extends AbstractEventSourceEntity
         implements AggregateRoot<OrderId> {
 
     private OrderId id;
@@ -110,16 +111,25 @@ public class Order extends AbstractEventSourceEntity
         this.verifyIfCanChangeToPlaced();
         this.changeStatus(OrderStatus.PLACED);
         this.setPlacedAt(OffsetDateTime.now());
+        publishDomainEvent(new OrderPlacedEvent(this.id(), this.customerId(), this.placedAt()));
     }
 
     public void markAsPaid() {
         this.changeStatus(OrderStatus.PAID);
         this.setPaidAt(OffsetDateTime.now());
+        publishDomainEvent(new OrderPaidEvent(this.id(), this.customerId(), this.paidAt()));
     }
 
     public void markAsReady() {
         this.changeStatus(OrderStatus.READY);
         this.setReadyAt(OffsetDateTime.now());
+        publishDomainEvent(new OrderReadyEvent(this.id(), this.customerId(), this.readyAt()));
+    }
+
+    public void cancel() {
+        this.changeStatus(OrderStatus.CANCELED);
+        this.setCanceledAt(OffsetDateTime.now());
+        publishDomainEvent(new OrderCanceledEvent(this.id(), this.customerId(), this.canceledAt()));
     }
 
     public void changePaymentMethod(PaymentMethod paymentMethod) {
@@ -166,11 +176,6 @@ public class Order extends AbstractEventSourceEntity
         this.items.remove(orderItem);
 
         this.recalculateTotals();
-    }
-
-    public void cancel() {
-        this.setCanceledAt(OffsetDateTime.now());
-        this.changeStatus(OrderStatus.CANCELED);
     }
 
     public boolean isDraft() {
